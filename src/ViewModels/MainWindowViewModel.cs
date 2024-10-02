4acc73fff4892fd01694ec9174b99b744fde760e
Terria-K <teuria@proton.me>
2024-10-02 21:45:30 +0800

    Separate SongViewModel from the MainViewModel

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -192,17 +192,3 @@ public partial class MainWindowViewModel : ViewModelBase
         }
     }
 }
-
-public partial class SongViewModel : ViewModelBase 
-{
-    public string Title => song.Title;
-    public string Artist => song.Artist;
-    public string Path => song.Path;
-    public string ArtworkURL => song.ArtworkUrl;
-
-    private Song song;
-    public SongViewModel(Song song) 
-    {
-        this.song = song;
-    }
-}
